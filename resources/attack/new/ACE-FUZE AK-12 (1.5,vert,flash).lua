6b0273e3bef4e80108652cb9f9ecfdb0a45572ac
gabe <137662751+incracy@users.noreply.github.com>
2023-08-04 19:27:01 -0700

    starting off the tree

---
+++ resources/attack/new/ACE-FUZE AK-12 (1.5,vert,flash).lua
@@ -0,0 +1,16 @@
+EnablePrimaryMouseButtonEvents(true);
+ 
+function OnEvent(event, arg)
+	if IsKeyLockOn("capslock" )then
+		if IsMouseButtonPressed(3)then
+			repeat	
+				if IsMouseButtonPressed(1) then
+					repeat
+						MoveMouseRelative(-1,5)
+						Sleep(9)
+					until not IsMouseButtonPressed(1)
+				end				
+			until not IsMouseButtonPressed(3)
+		end		
+	end
+end
