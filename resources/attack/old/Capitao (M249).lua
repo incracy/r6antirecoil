7c9b108d7d580455f0d3a491dd5db2eeadea6eb9
gabe <137662751+incracy@users.noreply.github.com>
2023-08-04 19:30:17 -0700

    Add files via upload

---
+++ resources/attack/old/Capitao (M249).lua
@@ -0,0 +1,17 @@
+EnablePrimaryMouseButtonEvents(true);
+ 
+function OnEvent(event, arg)
+	if IsKeyLockOn("capslock" )then
+		if IsMouseButtonPressed(3)then
+			repeat	
+				if IsMouseButtonPressed(1) then
+					repeat
+						MoveMouseRelative(-1,10)
+						Sleep(18)
+					until not IsMouseButtonPressed(1)
+				end				
+			until not IsMouseButtonPressed(3)
+		end		
+	end
+end
+
